c8a18af9ae26f0882527e504bd0ebfd6b7359357
rajushrestha1 <facebook101022222@gmail.com>
2026-06-30 17:11:33 +0545

    Remove custom redirect

--- next.config.mjs
+++ next.config.mjs
@@ -1,21 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  async redirects() {
-    return [
-      {
-        source: '/:path*',
-        has: [{ type: 'host', value: 'www.rajushrestha1.com.np' }],
-        destination: 'http://rajushrestha1.com.np/:path*',
-        permanent: true,
-      },
-    ];
-  },
-
   images: {
     remotePatterns: [
       {
-        protocol: 'https',
-        hostname: 'cdn.sanity.io',
+        protocol: "https",
+        hostname: "cdn.sanity.io",
       },
     ],
   },
